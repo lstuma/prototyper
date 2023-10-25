7ad31701e0b78a3c637ae9d73f3c39275e3734e2
lstuma <g.lstuma@gmail.com>
2023-10-25 09:03:25 +0200

    :children_crossing:

--- src/app/http_views.py
+++ src/app/http_views.py
@@ -1,4 +1,9 @@
 from http_utils import JSONRequest, JSONResponse
 
 def not_found(request: JSONRequest):
-    return JSONResponse(body={'error':f'the requested resource ({JSONRequest.path}) was not found'}, status=404)
+    return JSONResponse(body={'error':f'the requested resource ({request.path}) was not found'}, status=404)
+
+def welcome(request: JSONRequest):
+    return JSONResponse(body={'welcome':f'Prototyper'})
+
+# place your views here
